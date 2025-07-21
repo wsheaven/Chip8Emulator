96ecb7ac8dcb3c6416e22a0f95ee8a06097518f1
wsheaven <ws.heaven@gmail.com>
2025-07-21 13:49:15 -0700

    Created main structure of the program and general sdl code to create a window and display

---
+++ Chip8Emulator/Display.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SDL.h>
+#include <stdio.h>
+
+
+class Display
+{
+public:
+	Display(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
+
+
+	~Display();
+
+	void Update(void const* buffer, int pitch);
+
+
+
+private:
+	SDL_Window* window{};
+	SDL_Renderer* renderer{};
+	SDL_Texture* texture{};
+};
+
